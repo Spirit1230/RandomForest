b6a55f6b978619ad5fa15d67bf05372a1d8daa30
Spirit1230 <tom.osborn1@btinternet.com>
2021-05-16 14:58:06 +0100

    Start random forrest code

---
+++ RandomForrest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace MachineLearning 
+{
+    class RandomForrest 
+    {
+        List<DecisionTree> forrest = new List<DecisionTree>();
+
+        RandomForrest(string dataSource)  
+        {
+
+        }
+
+        public string GetDecision(string[] entryInput) 
+        {
+            string decision = "";
+
+            return decision;
+        }
+    }
+}
